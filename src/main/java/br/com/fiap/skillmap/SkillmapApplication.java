6107b27366f3b5a7af8cba5bc4653e840e53873d
GuiJanunzzi <guijanunzzi7@gmail.com>
2025-11-15 18:06:49 -0300

    Projeto criado

---
+++ src/main/java/br/com/fiap/skillmap/SkillmapApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.skillmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SkillmapApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SkillmapApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/com/fiap/skillmap/SkillmapApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.fiap.skillmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SkillmapApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
